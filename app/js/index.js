d59119addfd0dc9d5ac64261bf9663029fad861f
Fess292 <74976243+Fess292@users.noreply.github.com>
2021-08-16 21:40:06 +0300

    добавил вторую часть сайта

--- app/js/index.js
+++ app/js/index.js
@@ -2,3 +2,25 @@ import Swiper from "swiper";
 import 'swiper/swiper-bundle.css';
 
 const swiper = new Swiper();
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
